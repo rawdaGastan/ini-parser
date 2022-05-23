c53382d863928cc2828af203e6a7aee7bd7c72ef
rawdaGastan <rody.rawda.rr@gmail.com>
2022-05-23 12:40:27 +0200

    parser

---
+++ src/parse.py
@@ -0,0 +1,28 @@
+
+def ini_parse(file):
+    # read file lines
+    file_lines = file.readlines()
+
+    # parsed dict
+    parsed = {}
+
+    # for parents
+    new_parent = False
+
+    for line in file_lines:
+        if line[0] == "[":
+            parent = line[1: len(line) - 2]
+            parsed[parent] = {}
+            new_parent = True
+        
+        elif new_parent and "=" in line:
+            key, val = line.split(" = ")
+            val = val.replace("\n", "").replace("\r", "")
+
+            # check if digit
+            if val.isdigit():
+                val = int(val) 
+
+            parsed[parent][key] = val
+
+    return parsed
---
+++ test/test.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+
+#append src folder
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
+
+from parse import ini_parse
+
+# get the file location and name
+file_name = "sample.ini"
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+file = open(os.path.join(__location__, file_name))
+parsed = ini_parse(file)
+
+# test functions
+def test_value():
+    assert parsed["owner"]["name"] == "John", "Should be John"
+    assert parsed["owner"]["organization"] == "threefold", "Should be threefold"
+    assert parsed["database"]["server"] == "192.0.2.62", "Should be 192.0.2.62"
+    assert parsed["database"]["port"] == 143, "Should be 143"
+    assert parsed["database"]["password"] == 123456, "Should be 123456"
+
