0a752fb08c5fdaabd36cab13ca3e29dfc5af4466
rawdaGastan <rody.rawda.rr@gmail.com>
2022-05-24 21:39:32 +0200

    parser class

---
+++ src/ini_parser.py
@@ -0,0 +1,72 @@
+
+class Parser:
+
+  def __init__(self):
+    self.parsed_dict = {}
+    self.parsed_str = ""
+
+  def set_parsed_dict(self, parsed_dict):
+    self.parsed_dict = parsed_dict
+    self.get_sections()
+
+
+  def add_parent(self, parent):
+    # check if the parent does not exist in the parsed dict
+    if parent not in self.parsed_dict.keys():
+      self.parsed_dict[parent] = {}
+
+  def add(self, parent, key, value):
+    self.parsed_dict[parent][key] = value
+  
+
+  def to_string(self):
+    for parent, dict in self.parsed_dict.items():
+      self.parsed_str += "[" + str(parent) + "]\n"
+      for key, val in dict.items():
+        self.parsed_str += str(key) + " = " + str(val) + "\n"
+
+    return self.parsed_str
+
+
+  def from_string(self, content):
+    # read file lines
+    if(type(content) == str):
+      file_lines = content.splitlines()
+    else:
+      file_lines = content.readlines()
+
+    # for parents
+    new_parent = False
+
+    for line in file_lines:
+        line = line.replace("\n", "").replace("\r", "")
+
+        if len(line) > 0 and line[0] == "[" and line[len(line) - 1] == "]":
+            parent = line[1: len(line) - 1]
+            self.add_parent(parent)
+            new_parent = True
+        
+        elif len(line) > 0 and new_parent and line.count("=") == 1 and "=" != line[0] and line[0] != "":
+            if " = " in line:
+                key, val = line.split(" = ")
+            elif "=" in line:
+                key, val = line.split("=")
+
+            # check if digit
+            if val.isdigit():
+                val = int(val) 
+
+            self.add(parent, key, val)
+
+        elif len(line) > 0 and line[0] == ";" or line.strip() == '':
+            pass
+
+        else:
+            raise Exception("Not a valid ini file")
+
+    self.get_sections()
+    return self.parsed_dict
+
+
+  def get_sections(self):
+    self.sections = self.parsed_dict.keys()
--- test/test.py
+++ test/test.py
@@ -1,38 +1,39 @@
-import pytest
+#####################
+# append src folder #
+#####################
+
+import sys
 import os
 
-#append src folder
-import sys
+from pyparsing import unicodeString
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
 
-from parse import ini_parse
-from parsed import Parsed
+from ini_parser import Parser
 
 # get the file location and name
 file_name = "sample.ini"
-wrong_file_name = "sample wrong.ini"
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+
+##################
+# test functions #
+################## 
+
 def test_valid():
     file_content = open(os.path.join(__location__, file_name))
+    parser = Parser()
     try:
-        ini_parse(file_content)
+        parser.from_string(file_content)
         assert True
     except Exception as exc:
         assert False
 
-def test_in_valid():
-    file_content = open(os.path.join(__location__, wrong_file_name))
-    try:
-        ini_parse(file_content)
-        assert False
-    except Exception as exc:
-        assert True
 
 def test_value():
     file_content = open(os.path.join(__location__, file_name))
-    parsed = ini_parse(file_content)
+    parser = Parser()
+    parsed = parser.from_string(file_content)
     assert parsed["owner"]["name"] == "John", "Should be John"
     assert parsed["owner"]["organization"] == "threefold", "Should be threefold"
     assert parsed["database"]["server"] == "192.0.2.62", "Should be 192.0.2.62"
@@ -41,32 +42,81 @@ def test_value():
 
 def test_parsed_sections():
     file_content = open(os.path.join(__location__, file_name))
-    parsed = ini_parse(file_content)
+    parser = Parser()
+    parsed = parser.from_string(file_content)
 
-    test_parsed = Parsed()
-    test_parsed.set_parsed_dict(parsed)
+    test_parser = Parser()
+    test_parser.set_parsed_dict(parsed)
 
     # get sections
-    test_parsed.get_sections()
-    test_parsed_sections = test_parsed.sections
+    assert list(test_parser.sections) == list(parser.sections)
 
-    assert list(test_parsed_sections) == ['owner', 'database']
 
 def test_parsed_functions():
     file_content = open(os.path.join(__location__, file_name))
-    parsed = ini_parse(file_content)
+    parser = Parser()
+    parsed = parser.from_string(file_content)
 
-    test_parsed = Parsed()
+    test_parsed = Parser()
     test_parsed.set_parsed_dict(parsed)
+
     #parsed str
-    test_parsed.to_string()
-    test_parsed_str = test_parsed.parsed_str
+    test_parsed_str = test_parsed.to_string()
     
     # parsed dict
-    test_parsed.from_string(test_parsed_str)
-    test_parsed_dict = test_parsed.parsed_dict
+    test_parsed_dict = test_parsed.from_string(test_parsed_str)
 
     assert test_parsed_dict == parsed
 
-    
+##################
+# test invalid   #
+################## 
+
+def test_in_valid():
+    parser = Parser()
+    try:
+        parser.from_string("[owner]\n--")
+        assert False
+    except Exception as exc:
+        assert True
+
+def test_unclosed_section():
+    parser = Parser()
+    try:
+        parser.from_string("[owner\nkey=val\n")
+        assert False
+    except Exception as exc:
+        assert True
+
+def test_no_equal():
+    parser = Parser()
+    try:
+        parser.from_string("[owner]\nkeyval\n")
+        assert False
+    except Exception as exc:
+        assert True
+
+def test_no_value():
+    parser = Parser()
+    try:
+        parser.from_string("[owner]\nkeyval=\n")
+        assert False
+    except Exception as exc:
+        assert True
+
+def test_no_key():
+    parser = Parser()
+    try:
+        parser.from_string("[owner]\n=keyval\n")
+        assert False
+    except Exception as exc:
+        assert True
+
+def test_more_then_one_equal():
+    parser = Parser()
+    try:
+        parser.from_string("[owner]\nkey==val\n")
+        assert False
+    except Exception as exc:
+        assert True
 
